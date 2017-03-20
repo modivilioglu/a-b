723432f888fc650ad3ed6304fc21cc92623db453
mehmetoguz <mehmetoguzdivilioglu@mehmets-mbp.home>
2017-03-20 02:29:14 +0000

    Adding the service implementation, all test cases now pass.

--- src/main/scala/com/mod/addressbook/service/AddressBookServiceImpl.scala
+++ src/main/scala/com/mod/addressbook/service/AddressBookServiceImpl.scala
@@ -3,9 +3,21 @@ package com.mod.addressbook.service
 /**
   * Created by mehmetoguzdivilioglu on 20/03/2017.
   */
+
 import com.mod.addressbook.model._
-class AddressBookServiceImpl extends AddressBookService {
-  def getDayDifferenceByName(name1: Name, name2: Name): Option[Int] = ???
-  def getOldest: Person = ???
-  def getCountByGender(gender: Gender) = ???
+import com.mod.addressbook.dao.AddressBookDao
+import com.google.inject._
+
+class AddressBookServiceImpl @Inject()(val dao: AddressBookDao) extends AddressBookService {
+  def getDayDifferenceByName(name1: Name, name2: Name): Option[Int] = {
+    val difference = for {
+      person1 <- dao.getByName(name1.firstName)
+      person2 <- dao.getByName(name2.firstName)
+    } yield person1 |-?| person2
+    difference
+  }
+
+  def getOldest = dao.getOldest
+
+  def getCountByGender(gender: Gender) = dao.countByGender(gender)
 }
