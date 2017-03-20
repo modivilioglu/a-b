a90b972cff658b0971b670fdcb4c055056450286
mehmetoguz <mehmetoguzdivilioglu@mehmets-mbp.home>
2017-03-20 02:23:49 +0000

    Adding implementation for the Dao layer

--- src/main/scala/com/mod/addressbook/dao/AddressBookFlatFileDao.scala
+++ src/main/scala/com/mod/addressbook/dao/AddressBookFlatFileDao.scala
@@ -3,9 +3,26 @@ package com.mod.addressbook.dao
 /**
   * Created by mehmetoguzdivilioglu on 20/03/2017.
   */
+import scala.io.Source
 import com.mod.addressbook.model._
 object AddressBookFlatFileDao extends AddressBookDao {
-  def countByGender(gender: Gender): Int = ???
-  def getByName(firstName: String): Option[Person] = ???
-  def getOldest: Person = ???
+  val lines = Source.fromResource("address-book").getLines.toList
+  implicit val maxPerson: Ordering[Person] = new Ordering[Person] {
+    override def compare(p1: Person, p2: Person): Int = (p2 |-?| p1)
+  }
+
+  def extract(line: String): Person = line
+
+  val personList: List[Person] = lines.map(extract)
+
+  def countByGender(gender: Gender): Int = personList.count(person => person.gender == gender)
+
+  def getByName(firstName: String): Option[Person] = {
+    personList.filter(person => person.name.firstName == firstName) match {
+      case Nil => None
+      case firstPerson :: rest => Some(firstPerson)
+    }
+  }
+
+  def getOldest: Person = personList.max
 }
