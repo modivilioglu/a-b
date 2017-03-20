1bd29fd568c00178c0cb2c3cf3e6585936a4a465
mehmetoguz <mehmetoguzdivilioglu@mehmets-mbp.home>
2017-03-20 01:49:58 +0000

    Adding test cases for dao layer

--- src/main/scala/com/mod/addressbook/injection/InjectionModule.scala
+++ src/main/scala/com/mod/addressbook/injection/InjectionModule.scala
@@ -5,8 +5,10 @@ package com.mod.addressbook.injection
   */
 import com.google.inject._
 import com.mod.addressbook.service._
+import com.mod.addressbook.dao._
 class  InjectionModule extends AbstractModule {
   override def configure: Unit = {
+    bind(classOf[AddressBookDao]).toInstance(AddressBookFlatFileDao)
     bind(classOf[AddressBookService]).to(classOf[AddressBookServiceImpl])
   }
 }
---
+++ src/test/scala/com/mod/addressbook/dao/AddressBookDaoSpec.scala
@@ -0,0 +1,30 @@
+package com.mod.addressbook.dao
+
+/**
+  * Created by mehmetoguzdivilioglu on 20/03/2017.
+  */
+import com.mod.addressbook.injection._
+import com.mod.addressbook.model._
+import com.google.inject._
+import org.scalatest.{BeforeAndAfter, FlatSpec, Matchers}
+class AddressBookDaoSpec extends FlatSpec with BeforeAndAfter with Matchers {
+  val injector = Guice.createInjector(new InjectionModule)
+  val dao = injector.getInstance(classOf[AddressBookDao])
+  "Dao" should "give the correct number of Males" in {
+    dao.countByGender(Male) should be(3)
+  }
+  "Dao" should "give the correct number of Females" in {
+    dao.countByGender(Female) should be(2)
+  }
+  "Dao" should "give the oldest person" in {
+    val expectedName: Name = "Gemma Lane"
+    dao.getOldest.name shouldEqual expectedName
+  }
+  "Dao" should "get the person by name" in {
+    val expectedName: Name = "Gemma Lane"
+    dao.getByName("Gemma") match {
+      case None => fail()
+      case Some(person) => person.name shouldEqual expectedName
+    }
+  }
+}
